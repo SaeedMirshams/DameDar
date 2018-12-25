60d9cfc6b5a0d2aa9b4f4d2b3f4ab16c98fdd1a2
Mirshams <saeedMirshams@gmail.com>
2018-12-25 20:17:19 +0330

    add sources

---
+++ DameDar/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI.Popups;
+using System.Net.Http;
+using Windows.Web.Http;
+using Windows.Data.Json;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DameDar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("ارتباط با اینترنت برقرار نشد.");
+            dialog.Commands.Add(new UICommand("تلاش مجدد", new UICommandInvokedHandler(this.CommandInvokedHandler)));
+            dialog.Commands.Add(new UICommand("انصراف"));
+            await dialog.ShowAsync();
+        }
+
+        private async void CommandInvokedHandler(IUICommand command)
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            //Add a user-agent header to the GET request. 
+            var headers = httpClient.DefaultRequestHeaders;
+
+            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
+            //especially if the header value is coming from user input.
+            string header = "ie";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            Uri requestUri = new Uri("http://rest.iasco.ir/api/login?UserName=TEST&Password=TEST1");
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+                //httpResponse = await httpClient.GetAsync(requestUri);
+                var content = new HttpStringContent("Nothing");
+                httpResponse = await httpClient.PostAsync(requestUri, content);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+                var rootObject = JsonObject.Parse(httpResponseBody);
+                showMessage(rootObject["Code"].ToString());
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+        }
+
+        private async void showMessage(string message)
+        {
+            var dialog = new MessageDialog(message);
+            dialog.Title = "پیام";
+            dialog.Commands.Add(new UICommand("OK"));
+            await dialog.ShowAsync();
+
+        }
+    }
+}
